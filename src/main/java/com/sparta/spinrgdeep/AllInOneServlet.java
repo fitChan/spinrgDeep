be3a835df861d1c1a84f99ede3f0f9b584ddd3c1
fitChan <84306157+fitChan@users.noreply.github.com>
2021-07-02 12:04:37 +0900

    Initial commit

---
+++ src/main/java/com/sparta/spinrgdeep/AllInOneServlet.java
@@ -0,0 +1,2 @@
+package com.sparta.spinrgdeep;public class AllInOneServlet {
+}
---
+++ src/main/java/com/sparta/spinrgdeep/SpinrgDeepApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spinrgdeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpinrgDeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpinrgDeepApplication.class, args);
+    }
+
+}
